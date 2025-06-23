2d0245819fa5eb3d6eba92debb63763004a96aa0
Joanna Merson <jmerson@uoregon.edu>
2025-06-23 13:07:01 -0700

    data path

--- app/map.js
+++ app/map.js
@@ -384,7 +384,7 @@ function fillStateDataTable(){
 
 
 function fillDistricts(map, districtSourceId = 'oregon_districts', districtLayerId = 'district-fills') {
-  fetch('../assets/data/geojson/oregon_districts.geojson')
+  fetch('../assets/data/geojson/oregon_districts.geojson') // path that works for running from gitHub desktop for dev
     .then(response => response.json())
     .then(data => {
       data.features.forEach(feature => {
