0abbc0bf8df4a296d2bc5e21fb75f6999fb76dcd
Joanna Merson <jmerson@uoregon.edu>
2026-01-09 11:17:40 -0800

    alignment (smaller min map, factsheet scrolls right col only)

--- app/js/map.js
+++ app/js/map.js
@@ -1801,19 +1801,21 @@ function showDistrictFactsheet(clickedFeature, districtData) {
   // --- Build factsheet HTML (District) ---
   factSheetContainer.classList.add("full-width"); // full width top row
   factSheetContainer.innerHTML = `
-    <div class="opportunity-row"><h2>
+
+    <div class="opportunity-column">
+        <div class="opportunity-row">
+      <h2>
         <span class="factsheet-label">Factsheet for </span>
         <select id="districtPicker" class="district-dropdown">
           ${optionsHtml}
         </select>
 
-      </h2></div>
-    <div id="noGeometryNotice" class="no-geometry-notice" style="display:none;">
-      <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-      <strong>Note:</strong> This district cannot be found on our map. Available data is shown below.
+      </h2>
     </div>
-    <div class="opportunity-row">
-    <div class="opportunity-column">
+      <div id="noGeometryNotice" class="no-geometry-notice" style="display:none;">
+        <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+        <strong>Note:</strong> This district cannot be found on our map. Available data is shown below.
+      </div>
       <p><b>Latest information</b></p>
       <p>
         Teachers (FTE): ${fmtValue(latest.SCH_FTETEACH_TOT, latestYear)}<br>
@@ -1835,29 +1837,29 @@ function showDistrictFactsheet(clickedFeature, districtData) {
       <canvas id="compBar" width="300" height="100"></canvas>
     </div>
 
-    <div class="opportunity-column">
+    <div class="opportunity-column" id='factsheet-right'>
       <p><b>Historic/temporal information</b></p>
       <p style="font-size:0.9em;color: black">AP Participation Gap by Year</p>
       <canvas id="gapChart" width="400" height="160"></canvas>
       <div id="gapLegend" style="font-size:0.85em;"></div>
-  <!-- New sparkline: Students - Percentage of non-white students -->
-  <p style="font-size:0.9em;color: black">Students - Percentage of non-white students (by year)</p>
-  <canvas id="nonwhiteChart" width="400" height="160"></canvas>
-  <div id="nonwhiteLegend" style="font-size:0.85em;"></div>
-  <!-- New sparkline: HS students taking ≥1 AP (PCT_ENR_AP) -->
-  <p style="font-size:0.9em;color: black">HS students taking at least 1 AP course (by year)</p>
-  <canvas id="apChart" width="400" height="160"></canvas>
-  <div id="apLegend" style="font-size:0.85em;"></div>
-  <!-- New sparkline: Student-teacher ratio (STU_TEACH_RAT) -->
-  <p style="font-size:0.9em;color: black">Student–teacher ratio (by year)</p>
-  <canvas id="stChart" width="400" height="160"></canvas>
-  <div id="stLegend" style="font-size:0.85em;"></div>
-  <!-- New sparkline: Modal AP courses offered in district (SCH_APCOURSES) -->
-  <p style="font-size:0.9em;color: black">Modal number of AP courses offered (by year)</p>
-  <canvas id="apCoursesChart" width="400" height="160"></canvas>
-  <div id="apCoursesLegend" style="font-size:0.85em;"></div>
+      <!-- New sparkline: Students - Percentage of non-white students -->
+      <p style="font-size:0.9em;color: black">Students - Percentage of non-white students (by year)</p>
+      <canvas id="nonwhiteChart" width="400" height="160"></canvas>
+      <div id="nonwhiteLegend" style="font-size:0.85em;"></div>
+      <!-- New sparkline: HS students taking ≥1 AP (PCT_ENR_AP) -->
+      <p style="font-size:0.9em;color: black">HS students taking at least 1 AP course (by year)</p>
+      <canvas id="apChart" width="400" height="160"></canvas>
+      <div id="apLegend" style="font-size:0.85em;"></div>
+      <!-- New sparkline: Student-teacher ratio (STU_TEACH_RAT) -->
+      <p style="font-size:0.9em;color: black">Student–teacher ratio (by year)</p>
+      <canvas id="stChart" width="400" height="160"></canvas>
+      <div id="stLegend" style="font-size:0.85em;"></div>
+      <!-- New sparkline: Modal AP courses offered in district (SCH_APCOURSES) -->
+      <p style="font-size:0.9em;color: black">Modal number of AP courses offered (by year)</p>
+      <canvas id="apCoursesChart" width="400" height="160"></canvas>
+      <div id="apCoursesLegend" style="font-size:0.85em;"></div>
       
-    </div> <!-- end row -->
+
     </div>  <!-- end column -->
   `  ;
 
